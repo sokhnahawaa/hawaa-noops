c558b40885b91fcd4263221bf31ebd1970131d56
sokhnahawaa <157130515+sokhnahawaa@users.noreply.github.com>
2026-07-20 18:53:23 +0000

    feat: add main.tf [NoOpsSaaS]

---
+++ main.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "eu-west-1"
+}
+
+resource "aws_instance" "hawaa" {
+  ami           = "ami-0c55b159cbfafe1f0"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "hawaa"
+    Tech = "html"
+  }
+}
